7143373fd05cae2bab247bc36dc99b44eb845c6f
Alikhan <mukangaliev10@gmail.com>
2025-08-20 23:33:39 +0500

    changes in base.go

--- internal/http/handler/base.go
+++ internal/http/handler/base.go
@@ -16,37 +16,29 @@ func (h BaseHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte("OK"))
 		}(w, r)
 	case r.URL.Path == "/tasks" && r.Method == "GET":
-		{
 			tasks.GetTasks(w, r)
-		}
+	
 	case r.URL.Path == "/tasks" && r.Method == "POST":
-		{
 			tasks.PostTask(w, r)
-		}
+		
 	case strings.HasPrefix(r.URL.Path, "/tasks/") && r.Method == "GET":
-		{
 			tasks.GetTask(w, r)
-		}
+		
 	case strings.HasPrefix(r.URL.Path, "/tasks/") && r.Method == "PATCH":
-		{
 			tasks.PatchTask(w, r)
-		}
+			
 	case strings.HasPrefix(r.URL.Path, "/tasks/") && r.Method == "DELETE":
-		{
 			tasks.DeleteTask(w, r)
-		}
+		
 	case r.URL.Path == "/register" && r.Method == "POST":
-		{
 			auth.Register(w, r)
-		}
+	
 	case r.URL.Path == "/login" && r.Method == "POST":
-		{
 			auth.Login(w, r)
-		}
+
 	case r.URL.Path == "/logout" && r.Method == "POST":
-		{
 			auth.Logout(w, r)
-		}
+
 	default:
 		http.NotFound(w, r)
 	}
